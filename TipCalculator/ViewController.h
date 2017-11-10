6bf794666434d968e4491897aae0383301db9e3b
saintalexanderq <xanderquigley@gmail.com>
2017-11-10 08:37:46 -0500

    initial

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Alex Quigley on 2017-11-10.
+//  Copyright © 2017 Alex Quigley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
